2a3a740c582df3a938ffb87c2bff36a0d51e0356
Folashade-cloud <ftogunajo@gmail.com>
2022-11-30 06:22:05 -0800

    3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 number = 98
 street_name = "Battery street"
-print(f"Your address is, %s %s." % (number, street_name))
+#print(f"%s %s." % (number, street_name))
+print("{0} {1}" .format(number, street_name))
